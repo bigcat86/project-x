459f726332f63722713227c03c84a3b8192eca79
Aaron <attanner86@gmail.com>
2024-01-23 06:50:40 -0700

    atlas fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project-x', {
+mongoose.connect("mongodb+srv://attanner86:hbskater@clusterx.lxzybun.mongodb.net/?retryWrites=true&w=majority"
+|| 'mongodb://127.0.0.1:27017/project-x', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
