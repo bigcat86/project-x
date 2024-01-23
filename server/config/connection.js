36ff74ca2bd0116eaac071ca042db633bf293382
Aaron <attanner86@gmail.com>
2024-01-23 12:48:41 -0700

    help again

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,8 +6,8 @@ mongoose.connect(process.env.MONGODB_URI
 || 'mongodb://127.0.0.1:27017/project-x', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
